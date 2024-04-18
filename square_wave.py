0a4ad3328901c8145ca7d3fb985ca4d7ba77b0af
Mika <anghofus@angho.de>
2024-04-18 13:03:10 -0400

    added square_wave.py that creates images with alternating columns of pixels with y_min and y_max values

---
+++ square_wave.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image as im
+
+
+y_max = 64
+y_min = 0
+
+array = np.uint8(np.zeros((1152, 1920)))
+
+for i in range(1920):
+    if i % 2 == 0:
+        array[0][i] = y_max
+    else:
+        array[0][i] = y_min
+
+for j in range(1152):
+    array[j] = array[0]
+
+image = im.fromarray(array)
+image.show()
