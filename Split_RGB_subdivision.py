9af8a9f539142291450374a4001d17365412ea01
Mika <anghofus@angho.de>
2024-05-22 19:04:04 -0400

    moved

---
+++ Split_RGB_subdivision.py
@@ -0,0 +1,148 @@
+import numpy as np
+from PIL import Image
+import os
+from scipy import signal
+
+
+class PatternGeneration:
+    def __init__(self):
+        filepath = os.getcwd()
+        filename = "test.png"
+        image = Image.open(os.path.join(filepath, filename))
+
+        rgb_array = image_to_rgb_array(image)
+
+        # Define the dimensions and parameters for the pixel grid and waveforms
+        self.subpixel_width = 576
+        self.subpixel_height = 576
+        self.slm_width = 1920
+        self.slm_height = 1152
+        self.x_max_red = 38.91
+        self.x_max_green = 32.7
+        self.x_max_blue = 30
+        self.y_max = 128
+
+        # Initialize the list of subpixels and the main pixel grid
+        self.subpixel_list = []
+        self.pixel = np.zeros((self.slm_height, self.slm_width))
+
+    # Generate subdivided pixel patterns based on input colors
+    def subdivided_pixel(self, rgb_color: list):
+
+        """
+        Create subdivided pixels based on the given color list.
+
+        Parameters:
+        color (list): A list of 6 rgb colors.
+
+        Returns:
+        Image: A PIL image object representing the generated pixel image.
+        """
+
+        assert len(rgb_color) == 6, "color list must have 6 entries"
+
+        rgb_color = []
+
+        # Generate time array for the waveforms
+        t = np.linspace(0, self.subpixel_width, self.subpixel_width)
+
+        # Define the angular frequencies for the waveforms
+        omega_red = 2 * np.pi * 1 / self.x_max_red
+        omega_green = 2 * np.pi * 1 / self.x_max_green
+        omega_blue = 2 * np.pi * 1 / self.x_max_blue
+
+        # Generate the waveforms for each color channel
+        waveform_red = (1 + signal.sawtooth(omega_red * t)) * self.y_max / 2
+        waveform_green = (1 + signal.sawtooth(omega_green * t)) * self.y_max / 2
+        waveform_blue = (1 + signal.sawtooth(omega_blue * t)) * self.y_max / 2
+
+        # Generate subpixel patterns based on RGB percentages
+        for i in range(len(rgb_color)):
+            rgb = rgb_color[i]
+            subpixel = np.zeros((self.subpixel_width, self.subpixel_height))
+            red_width = rgb[0] * self.subpixel_width
+            green_width = rgb[1] * self.subpixel_width
+
+            j = 0
+            k = 0
+            # Assign waveform values to subpixel based on RGB widths
+            while j < red_width:
+                subpixel[0][j] = waveform_red[j]
+                j += 1
+            while j < red_width + green_width - 1:
+                subpixel[0][j] = waveform_green[j]
+                j += 1
+            while j < self.subpixel_width:
+                subpixel[0][j] = waveform_blue[j]
+                j += 1
+            while k < self.subpixel_height:
+                subpixel[k] = subpixel[0]
+                k += 1
+
+            self.subpixel_list.append(subpixel)
+            print(f"Subpixel {i} finished")
+
+        # Place the generated subpixels into the main pixel grid
+        for i in range(len(self.subpixel_list)):
+            self.place_subpixel(i)
+
+        # Convert the pixel grid to an image
+        image = Image.fromarray(self.pixel).convert("L")
+
+        return image
+
+    # Place subpixel patterns into the main pixel grid
+    def place_subpixel(self, i: int):
+
+        """
+        Place the subpixel in the correct position in the final image.
+
+        Parameters:
+        i (int): The index of the subpixel to be placed.
+        """
+
+        assert 0 <= i <= 5, "i can only be between 0 and 5"
+
+        current_subpixel = self.subpixel_list[i]
+
+        # Define coordinates for each subpixel placement
+        coordinates = [
+            (32, 0),  # for i == 0
+            (672, 0),  # for i == 1
+            (1312, 0),  # for i == 2
+            (32, 576),  # for i == 3
+            (672, 576),  # for i == 4
+            (1312, 576)  # for i == 5
+        ]
+
+        # Get the starting coordinates
+        x_coordinate, y_coordinate = coordinates[i]
+
+        # Place the current subpixel into the main pixel grid
+        for y in range(self.subpixel_height):
+            for x in range(self.subpixel_width):
+                target_x = x_coordinate + x
+                target_y = y_coordinate + y
+                self.pixel[target_y][target_x] = current_subpixel[y][x]
+
+
+def image_to_rgb_array(image):
+    image_split = image.split()
+
+    red_array = np.asarray(image_split[0])
+    green_array = np.asarray(image_split[1])
+    blue_array = np.asarray(image_split[2])
+
+    height, width = red_array.shape[:2]
+
+    rgb_array = np.zeros((height, width), dtype=object)
+
+    for y in range(height):
+        for x in range(width):
+            rgb_array[y][x] = (red_array[y][x], green_array[y][x], blue_array[y][x])
+
+    return rgb_array
+
+
+if __name__ == "__main__":
+    pass
---
+++ legacy/Oktay/display_folder.py
@@ -0,0 +1,409 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 09:11:38 2023
+
+@author: Oktay Senel
+
+this is a shell code to put any folder of images on the SLM. It does not create any images.
+
+This is (till now 4/2023) not an automatic code. This code will nee you to know what you want to do on the film.
+
+If you do not know how to code or understand code this will not be easy to make it suitable to your task.
+
+"""
+
+### Python libraries ###
+# Monitor packages
+from screeninfo import get_monitors # Screen Information (screeninfo) is a package to fetch location and size of physical screens.
+# Window packages 
+import tkinter as tk
+from tkinter import Toplevel, Tk, Label,filedialog # Graphical User Interface (GUI) package
+import PIL.Image, PIL.ImageTk # Python Imaging Librarier (PIL) package
+# Processing packages
+
+import numpy as np # Scientific computing package (NumPy)
+from PIL import Image as im #package for images 
+import glob #package to read a whole folder 
+import serial #package to be able to comunicate via serial
+import os# needed for filepaths 
+import time
+
+from exceptions import EquipmentError
+from exceptions import MotorError
+
+
+class Motor():
+    def __init__(self, port):
+        self.ser = serial.Serial()
+
+        self.ser.port = port
+        self.ser.baudrate = float(19200)
+        self.ser.timeout = float(.1)
+        self.ser.stopbits = float(1)
+        self.ser.bytesize = float(8)
+        self.ser.parity = 'N'
+        self.command_pause = .1
+
+        for axis in (1,2):
+            self.write_command(str(axis)+'MO')      # Turns Motor on
+            self.write_command(str(axis)+'VA'+'0.005')  # Sets velocity: this will be important for printing a grating 
+            self.write_command(str(axis)+'AC'+'4')  # Sets acceleration to 4
+            self.write_command(str(axis)+'AG'+'4')  # Sets deceleration to 4
+
+
+    def write_command(self, command:str):
+        if not self.ser.is_open:
+            self.ser.open()
+
+        if command.find('\r') == -1:
+            command += '\r'
+
+        try:
+            self.ser.write(command.encode())
+        except Exception as e:
+            message = ('Could not write this command to serial device:\n\t' + command)
+            raise EquipmentError(message, e)
+        time.sleep(self.command_pause)
+
+    def wait_motion_done(self, axis:int):
+        WAIT = .25
+        while True:
+            self.write_command(str(axis)+'MD?')
+            byte_info = self.ser.read(4)
+            try:
+                str_info = str(byte_info.decode())
+                if '1' in str_info:
+                    return
+                else:
+                    time.sleep(WAIT)
+            except Exception:
+                pass
+
+    def move_absolute(self, axis:int, go_to_position:float):
+        """
+        Move any axis to an absolute position.
+        """
+
+        self.write_command(str(axis)+'PA'+str(go_to_position))
+        self.wait_motion_done(axis)
+
+    def move_home(self, axis:int):
+        """
+        Move the motor to the home position.
+        """
+
+        self.write_command(str(axis)+'OR0')
+        self.wait_motion_done(axis)
+
+    def move_rel(self, axis:int, increment:float):
+        """
+        Move the axis to a relative position
+        """
+
+        self.write_command(str(axis)+'PR'+str(increment))
+        self.wait_motion_done(axis)
+        
+    def close(self):
+        self.ser.close()
+            
+        
+        
+
+class Shutter:
+    '''
+    ser: serial port (serial object)
+    '''
+    
+    ser = serial.Serial()
+    
+    def __init__(self, port, baudrate=9600, timeout=.1, stopbits=1, bytesize=8):
+        '''
+        Construct an object and configure the serial port.
+        #flow control = 1?
+        '''
+        
+        
+        self.ser.port = port
+        self.ser.baudrate = baudrate
+        self.ser.timeout = timeout
+        self.ser.stopbits = stopbits
+        self.ser.bytesize = bytesize
+        
+    def writeCommand(self, command, closeAfter=False):
+        '''
+        Send any command to a serial port.
+        (arg1) self 
+        (arg2) command: the command to send to the motor (string)
+        (arg3) closeAfter: close port after command, if true (boolean)
+        '''
+        
+        cmd = command
+        if cmd.find('\r') == -1:
+            cmd = cmd + '\r'
+        if self.ser.is_open == False:
+            self.ser.open()
+        self.ser.write(cmd.encode())
+        if closeAfter == True:
+            self.ser.close()
+            
+    def toggle_pause(self, pause):
+        '''
+        Opens the shutter. Pauses for a certain amount of time. Closes
+            the shutter.
+        (arg1) pause : number of seconds to pause (float)
+        '''
+        self.writeCommand('ens')
+        time.sleep(pause)
+        self.writeCommand('ens')
+
+    def toggle(self):
+        self.writeCommand('ens')
+
+    def startup(self):
+        response = self.writeCommand('ens?')
+        if response == "0":
+            self.writeCommand('ens')
+    def close(self):
+        self.ser.close()
+        
+      
+
+
+class SLM_window():
+
+    def __init__(self, master, grating = None):
+        ### Monitor controlling 
+        # Finds the resolution of all monitors that are connected.
+        active_monitors = get_monitors() # "monitor(screenwidth x screenheight + startpixel x + startpixel y)"
+        
+
+        # Assign the separated x and y start values to variables
+        begin_monitor_horizontal = active_monitors[0].x
+        begin_monitor_vertical = active_monitors[0].y
+        begin_slm_horizontal = active_monitors[1].x
+        begin_slm_vertical = active_monitors[1].y
+
+        #print(begin_slm_horizontal, begin_slm_vertical)
+        width = 1920
+        height = 1152
+
+        if grating is None:
+            array = np.zeros((height, width), dtype = np.uint16)
+            image = PIL.Image.fromarray(array)
+            image = image.convert('L')
+            grating = PIL.ImageTk.PhotoImage(image)
+
+        # self.image_window = Tk()
+        self.image_window = master
+        
+
+        # Create a window on the screen of the SLM monitor
+        self.window_slm = Toplevel(self.image_window)
+        self.window_slm_geometry = str("{:}".format(width) + 'x' + "{:}".format(height) + '+' + "{:}".format(begin_slm_horizontal) + '+' + "{:}".format(begin_slm_vertical))
+        
+        #print(self.window_slm_geometry)
+        self.window_slm.geometry(self.window_slm_geometry)
+        self.window_slm.overrideredirect(1)
+        
+        
+
+        # Load the opened image into the window of the SLM monitor
+        
+        self.window_slm_label = Label(self.window_slm,image=grating)
+        self.window_slm_label.image = grating
+        self.window_slm_label.pack()
+        
+        # Termination command for the code
+        self.window_slm.bind("<Escape>", lambda e: self.window_slm.destroy())
+       
+    
+    def display(self,grating):
+        self.window_slm_label.config(image=grating)
+        self.window_slm_label.image = grating
+        
+    def display_text(self,msg):
+        self.window_slm_label.config(text=msg)
+
+    def close_window(self):
+        self.window_slm.destroy()
+        self.window_slm.update()
+    
+    
+def next_image():
+    global i
+    global slm
+    global image_list
+    slm.close_window()
+    slm = SLM_window(root)
+    slm.display(image_list[i])
+    print("you are showing image "+ str(names[i]))
+    i=i+1
+
+def prev_image():
+    global i
+    global slm
+    global image_list
+    slm.close_window()
+    slm = SLM_window(root)
+    slm.display(image_list[i-2])
+    print("you are showing image "+ str(names[i-2]))
+    i=i-1
+    
+
+    
+def open_images():
+    global image_list
+    global names
+    global slm
+    filepath=filedialog.askdirectory()
+    image_list=[]
+    names=[]
+    for filename in glob.glob(filepath + "/*.png"):
+        image=im.open(filename)
+        image=PIL.ImageTk.PhotoImage(image)
+        names.append(os.path.basename(filename))
+        image_list.append(image)
+    print("you are showing " +str(names[0])+ " please make sure that the images are read in the order you want them to be")
+    slm=SLM_window(root)
+    slm.display(image_list[0])
+    
+#define your movement here; most changes will be made here (do not forget to set your velocity)
+#while the movement is beeing done you can not change the image on the SLM so far (4/2023)
+def movement1():
+    motor = Motor('COM25')
+    shutter = Shutter("COM24")
+    print("movement started")
+    shutter.toggle()
+    motor.move_rel(2,1)
+    shutter.toggle()
+    motor.move_rel(1,-0.12)
+    motor.close()
+    shutter.close()
+    print("movement done next move should be -")
+    
+    
+def movement2():
+    motor = Motor('COM25')
+    shutter = Shutter("COM24")
+    print("movement started")
+    shutter.toggle()
+    motor.move_rel(2,-1)
+    shutter.toggle()
+    motor.move_rel(1,-0.12)
+    motor.close()
+    print("movement done next move should be +")
+    shutter.close()
+    
+    
+def new_matrix():
+    motor = Motor('COM25')
+    print("movement started")
+    motor.move_rel(1,-0.5)
+    motor.close()
+    print("movement done nextmatrix can be printed")
+    shutter.close()
+    
+def stichdown():
+    global exp_time
+    motor = Motor('COM25')
+    shutter = Shutter("COM24")
+    motor.move_rel(2,0.08)
+    motor.wait_motion_done(2)
+    shutter.toggle()
+    time.sleep(exp_time)
+    shutter.toggle()
+    motor.close()
+    shutter.close()
+    
+def stichup():
+    global exp_time
+    motor = Motor('COM25')
+    shutter = Shutter("COM24")
+    motor.move_rel(2,-0.08)
+    motor.wait_motion_done(2)
+    shutter.toggle()
+    time.sleep(exp_time)
+    shutter.toggle()
+    motor.close()
+    shutter.close()
+    
+def newline():
+    motor = Motor('COM25')
+    shutter = Shutter("COM24")
+    motor.move_rel(1,-0.12)
+    motor.wait_motion_done(1)
+    shutter.toggle()
+    time.sleep(exp_time)
+    shutter.toggle()
+    shutter.close()
+    motor.close()
+    
+def put_that_on_there():
+    global names
+    n=int(len(names)**0.5)
+    print(n)
+    print("stitching started a "+str(n) +"x"+str(n)+" grating")
+    j=0
+    k=500
+    start=0
+    nn=len(names)
+    o=0
+    while j<nn :
+        if start==0 and j<nn:
+             shutter = Shutter("COM24")
+             shutter.toggle()
+             time.sleep(exp_time)
+             shutter.toggle()
+             shutter.close()
+             start=1
+             j=j+1
+        if o<n-1 and j<nn:
+            next_image()
+            root.update()
+            stichdown()
+            o=o+1
+            j=j+1        
+        if o==n-1 and j<nn:
+            k=0
+            o=500
+            next_image()
+            root.update()
+            newline()
+            j=j+1
+        if k<n-1 and j<nn :
+            next_image()
+            root.update()
+            stichup()
+            k=k+1
+            j=j+1
+        if k==n-1 and j<nn :
+            k=500
+            o=0
+            next_image()
+            root.update()
+            newline()
+            j=j+1
+    print("stitching done")
+    
+root = tk.Tk()
+global i
+global exp_time
+exp_time=5
+i=1 
+btn0=tk.Button(root,text="read",bd="5", command=open_images)
+btn0.pack(side="left")
+btn2=tk.Button(root,text="next", bd="5", command=next_image)
+btn2.pack(side="left")
+btn7=tk.Button(root,text="prev",bd="5", command=prev_image)
+btn7.pack(side="left")
+btn3=tk.Button(root,text="print +", bd="5", command= movement1)
+btn3.pack(side="left")
+btn3=tk.Button(root,text="print -", bd="5", command= movement2)
+btn3.pack(side="left")
+btn6=tk.Button(root,text="new matrix", bd="5", command= new_matrix)
+btn6.pack(side="left")
+btn5=tk.Button(root,text="stich",bd="5", command=put_that_on_there)
+btn5.pack(side="left")
+btn4=tk.Button(root,text="end",bd="5",command=root.destroy)
+btn4.pack(side="left")
+root.mainloop()
