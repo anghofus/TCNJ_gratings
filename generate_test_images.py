3d4ec4570076657179909dff3644d59ecde0a87d
Mika <anghofus@angho.de>
2024-05-22 17:50:17 -0400

    bug fixes

--- generate_test_images.py
+++ generate_test_images.py
@@ -5,16 +5,17 @@ red = 255
 green = 0
 blue = 0
 
-height = 10
+height = 20
 width = 10
 
 image_array = np.zeros((height, width, 3), dtype=np.uint8)
+print(image_array.shape)
 
 for y in range(height):
     for x in range(width):
-        image_array[x][y][0] = red
-        image_array[x][y][1] = green
-        image_array[x][y][2] = blue
+        image_array[y][x][0] = red
+        image_array[y][x][1] = green
+        image_array[y][x][2] = blue
 
 image = Image.fromarray(image_array, "RGB")
 image.save('test.png')
