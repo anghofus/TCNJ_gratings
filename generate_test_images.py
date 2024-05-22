ed1209fc07af106b874d08ac8a60a9b40109fbb3
Mika <anghofus@angho.de>
2024-05-22 12:46:12 -0400

    added generate_test_images.py, a simple python script to generate one color test images with a customizable size and color

---
+++ generate_test_images.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import numpy as np
+
+red = 255
+green = 0
+blue = 0
+
+height = 10
+width = 10
+
+image_array = np.zeros((height, width, 3), dtype=np.uint8)
+
+for y in range(height):
+    for x in range(width):
+        image_array[x][y][0] = red
+        image_array[x][y][1] = green
+        image_array[x][y][2] = blue
+
+print(image_array)
+image = Image.fromarray(image_array, "RGB")
+image.save('test.png')
