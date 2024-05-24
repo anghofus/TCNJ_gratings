c0e19e92bac92e6f7f789cddd9ffd05507b4c5a4
Mika <anghofus@angho.de>
2024-05-24 17:13:20 -0400

    added the create_csv methode and the calculate_coordinates methode from julian's code

--- Mika/Split_RGB_subdivision.py
+++ Mika/Split_RGB_subdivision.py
@@ -2,11 +2,15 @@ import numpy as np
 from PIL import Image
 import os
 from scipy import signal
+import csv
 
 
 class PatternGeneration:
     def __init__(self):
         # Define the dimensions and parameters for the pixel grid and waveforms
+        self.filepath = os.getcwd()
+        filename_image = "test.png"
+
         self.subpixel_width = 576
         self.subpixel_height = 576
 
@@ -15,6 +19,7 @@ class PatternGeneration:
 
         self.slm_width = 1920
         self.slm_height = 1152
+        self.slm = 484
 
         self.x_max_red = 38.91
         self.x_max_green = 32.7
@@ -25,13 +30,12 @@ class PatternGeneration:
         self.subpixel_list = []
         self.pixel = np.zeros((self.slm_height, self.slm_width))
 
-        filepath = os.getcwd()
-        filename = "test.png"
-        image = Image.open(os.path.join(filepath, filename))
+        image = Image.open(os.path.join(self.filepath, filename_image))
 
         if image.size > (270, 270):
             raise Exception("Image must have a resolution auf 270x270 or less")
 
+        self.image_width, self.image_height = image.size
         self.rgb_array = image_to_rgb_array(image)
         self.pixel_list = self.rgb_array_to_pixel_list(self.rgb_array)
         self.added_RGB_values = self.generate_added_RGB_values()
@@ -160,6 +164,67 @@ class PatternGeneration:
             added_RGB_values.append(average)
 
         return added_RGB_values
+
+    # Function to calculate coordinates in the snake pattern
+    def calculate_coordinates(self, rows, columns, slm):
+        points = []
+        start_x = ((columns / 2) - 1) * slm + slm / 2
+        start_y = -(((rows / 2) - 1) * slm + slm / 2)
+
+        for row in range(rows):
+            for col in range(columns):
+                points.append((start_x, start_y))
+
+                # Update x-coordinate based on row parity and not at the end of the row
+                if col < columns - 1:
+                    start_x -= slm if (row % 2 == 0) else 0
+                    start_x += slm if (row % 2 != 0) else 0
+
+            # Update y-coordinate at the end of each row
+            start_y += slm * (145 / 242)
+
+        return points
+
+    def create_csv(self):
+        # Create a new CSV file and write the added_rgb_values to it
+        filename_added_RGB = "added_RGB_values.csv"
+        added_RGB_values_path = os.path.join(self.filepath, filename_added_RGB)
+        # added_RGB_values_path = "C:/Users/mcgeelab/Desktop/SLMImages/added_RGB_values.csv"
+
+        with open(added_RGB_values_path, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["addedRGB"])  # Header row
+
+            for value in self.added_RGB_values:
+                csv_writer.writerow([value])
+
+        # Specify the input and output file paths
+        filename_dataset = "dataset.csv"
+        dataset_path = os.path.join(self.filepath, filename_dataset)
+        # dataset_path = 'C:/Users/mcgeelab/Desktop/SLMImages/dataset.csv'
+
+        # Calculate new coordinates without relying on added_RGB_values.csv
+        coordinates = self.calculate_coordinates(self.image_height, self.image_width, self.slm)
+
+        # Create a new list to store the modified data
+        modified_data = []
+
+        # Ensure that only the second and third columns are extended with the calculated coordinates
+        for i in range(len(coordinates)):
+            # Include exp_times in the first column and update the second and third columns
+            modified_data.append([self.added_RGB_values[i]] + [int(coord) for coord in coordinates[i]])
+            # + list(coordinates[i]))
+
+        # Open the CSV file for writing
+        with open(dataset_path, 'w', newline='') as output_file:
+            # Write the header row
+            writer = csv.writer(output_file)
+            writer.writerow(["addedRGB", "X", "Z"])
+            # Write the modified data
+            writer.writerows(modified_data)
+
+        print("Both CSV-Sheets have been created")
+
 
 def image_to_rgb_array(image):
     image_split = image.split()
