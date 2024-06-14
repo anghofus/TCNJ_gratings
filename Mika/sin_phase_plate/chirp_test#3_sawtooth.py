7d13f6228816657a09fe9f7d474de75b14abb13d
Mika <anghofus@angho.de>
2024-06-14 11:41:02 -0400

    folder renamed

--- Mika/sin_phase_plate/chirp_test#3_sawtooth.py
+++ Mika/sin_phase_plate/chirp_test#3_sawtooth.py
@@ -8,6 +8,9 @@ focal_length = 10 * 10 ** -3  # in m
 wavelength = 633 * 10 ** -9  # in m
 slm_width = 200 * 10 ** -6  # in m
 
+x_peak_to_peak = 128
+x_min = 20
+
 slm_count = int(radius / slm_width)
 linespace_width = int(slm_count * 1920)
 pixel_width = radius / linespace_width
@@ -17,7 +20,7 @@ waveform = []
 
 
 def transmission_function(r):
-    f = scipy.signal.sawtooth(math.radians((2*np.pi)/(focal_length*wavelength)*r**2))
+    f = x_min + (1+scipy.signal.sawtooth(math.radians((2*np.pi)/(focal_length*wavelength)*r**2)))/2 * x_max
     return f
 
 
