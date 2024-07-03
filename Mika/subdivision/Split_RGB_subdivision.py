8bf0ebd0ca08230b84f0f58b6bcfcb61689bddec
Mika <anghofus@angho.de>
2024-07-03 14:57:14 -0400

    implemented the rgb_array_to_pixel_list-method (and therefor added the get_subpixel_color-method) from pixel_list_snake_pattern_test.py in the Split_RGB_subdivision.py

--- Mika/subdivision/Split_RGB_subdivision.py
+++ Mika/subdivision/Split_RGB_subdivision.py
@@ -164,18 +164,27 @@ class PatternGeneration:
         pixel_list = []
 
         # Iterate over the RGB array in steps of pixel_height and pixel_width
-        for rgb_array_y in range(0, rgb_array.shape[0] - 1, self.pixel_height):
-            for rgb_array_x in range(0, rgb_array.shape[1] - 1, self.pixel_width):
-                sub_pixel_color = []
-                # Collect the colors for each subpixel
-                for sub_pixel_y in range(self.pixel_height):
-                    for sub_pixel_x in range(self.pixel_width):
-                        target_y = rgb_array_y + sub_pixel_y
-                        target_x = rgb_array_x + sub_pixel_x
-                        sub_pixel_color.append(rgb_array[target_y][target_x])
-                pixel_list.append(sub_pixel_color)
+        for i, rgb_array_y in enumerate(range(rgb_array.shape[0], 0, - self.pixel_height)):
+            if i % 2 == 0:
+                for rgb_array_x in range(rgb_array.shape[1] - 1, 0, -self.pixel_width):
+                    sub_pixel_color = self.get_subpixel_color(rgb_array, rgb_array_x, rgb_array_y)
+                    pixel_list.append(sub_pixel_color)
+            else:
+                for rgb_array_x in range(0, rgb_array.shape[1] - 1, self.pixel_width):
+                    sub_pixel_color = self.get_subpixel_color(rgb_array, rgb_array_x, rgb_array_y)
+                    pixel_list.append(sub_pixel_color)
 
         return pixel_list
+
+    def get_subpixel_color(self, rgb_array, rgb_array_x, rgb_array_y):
+        sub_pixel_color = []
+        # Collect the colors for each subpixel
+        for sub_pixel_y in range(self.pixel_height, 0, -1):
+            for sub_pixel_x in range(self.pixel_width, 0, -1):
+                target_y = rgb_array_y - sub_pixel_y
+                target_x = rgb_array_x - sub_pixel_x
+                sub_pixel_color.append(rgb_array[target_y][target_x])
+        return sub_pixel_color
 
     def generate_added_RGB_values(self):
         """
