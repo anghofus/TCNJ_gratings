22c2468494ed1aab29aea94aed463240ce2ec850
Mika <anghofus@angho.de>
2024-07-16 18:18:33 -0400

    fixed scrambled subpixel bug

--- Mika/subdivision/Split_RGB_subdivision.py
+++ Mika/subdivision/Split_RGB_subdivision.py
@@ -14,7 +14,7 @@ class PatternGeneration:
         # Set the file path and image filename
         self.filepath_output = os.getcwd()
         self.filepath_input = os.getcwd()
-        filename_image = "test2.png"
+        filename_image = "test4.png"
 
         # Define dimensions for subpixels and SLM (spatial light modulator)
         self.subpixel_width = 640
@@ -193,22 +193,28 @@ class PatternGeneration:
         for i, rgb_array_y in enumerate(range(rgb_array.shape[0], 0, - self.pixel_height)):
             if i % 2 == 0:
                 for rgb_array_x in range(rgb_array.shape[1] - 1, 0, -self.pixel_width):
-                    sub_pixel_color = self.get_subpixel_color(rgb_array, rgb_array_x, rgb_array_y)
+                    sub_pixel_color = self.get_subpixel_color(rgb_array, rgb_array_x, rgb_array_y, 0)
                     pixel_list.append(sub_pixel_color)
             else:
                 for rgb_array_x in range(0, rgb_array.shape[1] - 1, self.pixel_width):
-                    sub_pixel_color = self.get_subpixel_color(rgb_array, rgb_array_x, rgb_array_y)
+                    sub_pixel_color = self.get_subpixel_color(rgb_array, rgb_array_x, rgb_array_y, 1)
                     pixel_list.append(sub_pixel_color)
 
         return pixel_list
 
-    def get_subpixel_color(self, rgb_array, rgb_array_x, rgb_array_y):
+    def get_subpixel_color(self, rgb_array, rgb_array_x, rgb_array_y, i):
+
         sub_pixel_color = []
         # Collect the colors for each subpixel
         for sub_pixel_y in range(self.pixel_height, 0, -1):
             for sub_pixel_x in range(self.pixel_width, 0, -1):
                 target_y = rgb_array_y - sub_pixel_y
-                target_x = rgb_array_x - sub_pixel_x
+                if i == 1:
+                    target_x = 3 + rgb_array_x - sub_pixel_x
+                elif i == 0:
+                    target_x = 1 + rgb_array_x - sub_pixel_x
+                else:
+                    raise ValueError
                 sub_pixel_color.append(rgb_array[target_y][target_x])
         return sub_pixel_color
 
