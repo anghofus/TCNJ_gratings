b6710a13f781509e8c014b979789989686ed3fd6
Mika <anghofus@angho.de>
2024-08-01 17:19:06 -0400

    started the motor control program

---
+++ Mika/sin_phase_plate/esp_controller.py
@@ -0,0 +1,13 @@
+import serial
+
+
+class ESPController:
+    def __init__(self, port:str):
+        self.ser = serial.Serial(
+            port=port,
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            rtscts=True
+        )
